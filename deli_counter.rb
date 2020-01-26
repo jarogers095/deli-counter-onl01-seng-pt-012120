6115e7f98297f9d6475351d90e58a0411a8797ef
James Rogers <jarogers095@gmail.com>
2020-01-26 10:35:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(the_line)
+  if (the_line > 0)
+    message = "The line is currently:"
+    the_line.each_with_index do |index,customer|
+      message << " #{index+1}. #{customer}"
+    end
+  else
+    message = "The line is currently empty."
+  end
+  
+  return message
+end
+  
+  
