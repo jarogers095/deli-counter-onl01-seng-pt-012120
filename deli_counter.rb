e75e288e9119f5a57d996848e68fd16e5ddd9eb0
James Rogers <jarogers095@gmail.com>
2020-01-26 10:42:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,4 +11,20 @@ def line(the_line)
   puts message
 end
   
+def take_a_number(the_line, name)
+  the_line.push(name)
+  puts "Welcome, #{name}. You are number #{the_line.size} in line."
+  return the_line
+end
+
+def now_serving(the_line)
+  if (the_line.size > 0)
+    puts "Currently serving #{the_line[0]}"
+    the_line.shift
+  else 
+    puts "There is nobody waiting to be served!"
+  end
+  
+  return the_line
+end
   
